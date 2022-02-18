57d2eee9cdf611f8182cc499c9704cdcaa9ac6bc
prakashmhaske <prakash.mhaske@waiin.com>
2022-02-18 17:42:47 +0530

    End Of Section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,34 +16,33 @@ namespace API.Controllers
 {
   
 
-   [ApiController]
+    [ApiController]
     [Route("api/[controller]")] 
+        [Authorize]
         public class UsersController : ControllerBase
     {
-        private readonly DataContext _context;
+        
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
 
-        public UsersController(DataContext context)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            _mapper = mapper;
+            _userRepository = userRepository;
         }
 
         [HttpGet]
-        // [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<AppUser>>>GetUsers()
+       
+        public async Task<ActionResult<IEnumerable<MemberDto>>>GetUsers()
         {
-            
-            return await _context.Users.ToListAsync();
-           
-           
+            var users = await _userRepository.GetMemberAsync();
+            return Ok(users);
         }
-        //api/users/3
- 
-        [HttpGet("{id}")]
-        public async Task< ActionResult<AppUser>>GetUser(int id)
+        
+        [HttpGet("{username}")]
+        public async Task< ActionResult<MemberDto>>GetUser(string username)
         {
-            return await _context.Users.FindAsync(id);  
-
-            
+            return await _userRepository.GetMemberAsync(username);
            
         }
 
