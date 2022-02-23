45d5c3e52e460c30d4640e574bba6f75bedc8f8f
prakashmhaske <prakash.mhaske@waiin.com>
2022-02-23 11:46:11 +0530

    End Of Section 9

--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -4,3 +4,5 @@ export interface User
   token:string;
 }
 
+
+
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
     children:[
 
       {path:'members', component: MemberListComponent, canActivate: [AuthGuard]},
-      {path:'members/:id', component: MemberDetailComponent},
+      {path:'members/:username', component: MemberDetailComponent},
       {path:'lists',component: ListComponent},
       {path:'messages',component: MessagesComponent},
 
@@ -31,7 +31,6 @@ const routes: Routes = [
   {path:'**',component: NotFoundComponent,pathMatch:'full'},
 
 ];
-
 
 
 @NgModule({
